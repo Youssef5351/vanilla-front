acba6c7ed4d5d3150350e8fa874de5b037645202
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-08-20 20:37:10 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'https://vanilla6.vercel.app',
+  baseURL: 'https://vanilla6.vercel.app/',
   withCredentials: true 
 });
 
